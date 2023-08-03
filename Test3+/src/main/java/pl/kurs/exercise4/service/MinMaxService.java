08065facd4c754014f0d7241dd0f9a0a166dcdff
Wiktor <123586435+WiktorSzz@users.noreply.github.com>
2023-08-03 12:43:35 +0200

    Update MinMaxService.java

--- Test3+/src/main/java/pl/kurs/exercise4/service/MinMaxService.java
+++ Test3+/src/main/java/pl/kurs/exercise4/service/MinMaxService.java
@@ -3,31 +3,6 @@ package pl.kurs.exercise4.service;
 import pl.kurs.exercise4.models.MinMax;
 
 import java.util.List;
-
-//    public static <T extends Comparable<T>> MinMax<T> getMinAndMax(List<T> elements) {
-//        if (elements == null || elements.isEmpty()) {
-//            throw new NullPointerException("Lista jest pusta lub zawiera elementy null!");
-//        }
-//        T min = null;
-//        T max = null;
-//
-//        for (T element : elements) {
-//            if (element == null) {
-//                continue;
-//            }
-//
-//            if (min == null || element.compareTo(min) < 0) {
-//                min = element;
-//            }
-//            if (max == null || element.compareTo(max) > 0) {
-//                max = element;
-//            }
-//        }
-//
-//        return new MinMax<>(min, max);
-//    }
-
-
 import java.util.Objects;
 
 public class MinMaxService {
